e2635535bc8cdc041afd97f1ab7f0e21da5b2803
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2018-01-28 23:35:31 +0100

    Added serverless example

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const Database = require('./database')
 const port = 42481
 
-const database = new Database()
+// const database = new Database()
 const app = express()
 const server = http.createServer(app);
 
@@ -13,25 +13,25 @@ app.get('/', function(req, res) {
   res.sendFile('index.html', { root: './' })
 })
 
-app.get('/games/list', function (req, res) {
-  res.send(database.games)
-})
+// app.get('/games/list', function (req, res) {
+//   res.send(database.games)
+// })
 
-app.post('/games/list/:game/:price', function (req, res) {
-  let title = req.params.game
-  let price = req.params.price
+// app.post('/games/list/:game/:price', function (req, res) {
+//   let title = req.params.game
+//   let price = req.params.price
 
-  database.add(title, price).then(() => {
-    res.send(database.games)
-  })
-})
+//   database.add(title, price).then(() => {
+//     res.send(database.games)
+//   })
+// })
 
-app.delete('/games/list/:id', function (req, res) {
-  let gameId = req.params.id
+// app.delete('/games/list/:id', function (req, res) {
+//   let gameId = req.params.id
 
-  database.delete(gameId).then(() => {
-    res.send(database.games)
-  })
-})
+//   database.delete(gameId).then(() => {
+//     res.send(database.games)
+//   })
+// })
 
 server.listen(port);
